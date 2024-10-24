87474b50948c3fe3090d68b89c1cf7cea36f4c8d
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-10-24 10:37:10 +0300

    remove customAgents tracking and add it in setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+pip install -r requirements.txt
+
+cd chat-hereiz/
+
+if [ -d "customAgents" ] || [ -d "tests" ]; then
+    echo "Removing existing customAgents and tests..."
+    rm -rf customAgents* tests*
+fi
+
+echo "Getting customAgents..."
+git clone https://github.com/Ahmed-Hereiz/customAgents.git
+
+cd customAgents/scripts
+bash setup.sh
+cd ../../
+
+mv customAgents tmpcustomAgents
+mv tmpcustomAgents/customAgents* .
+mv tmpcustomAgents/tests .
+
+rm -rf tmpcustomAgents
+
+exit 0
